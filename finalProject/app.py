669479e4d5ec0ebd51ee3158893aab41e426d51b
Ken <sithivitken55@gmail.com>
2020-03-27 06:54:19 +0000

    change in index.html layout

--- finalProject/app.py
+++ finalProject/app.py
@@ -1,6 +1,5 @@
 import os
 
-from datetime import date
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_socketio import SocketIO, send
@@ -8,7 +7,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from ChatBot.chatbot import chatbot
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
@@ -50,6 +49,13 @@ if not os.environ.get("API_KEY"):
 def handlwMessage(msg):
     print("Message: " + msg)
     send(msg, broadcast=True)
+
+
+@socketio.on("message")
+def handlwResponse(msg):
+    response = chatbot(msg)
+    print(response["text"])
+    send(response["text"], broadcast=True)
 
 
 @app.route("/")
